1a6f583c8e94577f58bff853c1f93109831d8a75
woshihaoren123 <395668455@qq.com>
2020-12-10 14:53:53 +0800

    Update WechatRedirect.js

--- WechatRedirect.js
+++ WechatRedirect.js
@@ -2,14 +2,14 @@
     initialize: function(component, event, helper) {
         var query = window.location.search.substring(1);   
         var vars = query.split("&");
-        for (var i=0;i<vars.length;i++) {
+        for (var i = 0; i < vars.length; i++) {
             var pair = vars[i].split("=");
-            if(pair[0] == 'st'){
-                helper.redirectToGetCode(component, pair[1]);
+            if(pair[0] == 'stp'){
+                helper.saveState(component, pair[1]);
                 return ;
             }
             if(pair[0] == 'state'){
-                helper.redirectToGetToken(component, pair[1]);
+                helper.getState(component, pair[1]);
                 return ;
             }
         }
